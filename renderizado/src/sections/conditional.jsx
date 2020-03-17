b9f5cc4619bed3e19c19c814a8e50b42813dd315
Leonardo Medina <leoz.31@hotmail.com>
2020-03-16 21:19:19 -0500

    rederizado objetos

--- renderizado/src/sections/conditional.jsx
+++ renderizado/src/sections/conditional.jsx
@@ -1,18 +1,48 @@
 import React, {Component} from 'react';
+import cars from '../data/cars.json'
+
+class CarItem extends Component {
+    render() {
+        const { car } = this.props;
+        return (
+            <li >
+                <p> <strong>Nombre:</strong>{car.name}</p>
+                <p> <strong>Marca:</strong>{car.company}</p>
+            </li>
+        )
+    }
+}
+
 class ComponentA extends Component {
     render() {
+        const numbers = [1,1,3,4,5]
         return (
             <div>
-                COMPONENTE A
+                <button>Iniciar Sesión</button>
+                <h4>Trabajando con listas </h4>
+                {numbers.map((num, index )=>{
+                    return <p key={index} > Soy el numero {num}</p>
+                })}
+                <h4> Trabajando listas con objetos</h4>
+                <ul>
+                    {
+                        cars.map( car => {
+                                return <CarItem key={car.id} car={car}></CarItem>
+                        })
+                    }
+                </ul>
             </div>
         );
     }
 }
+
+
 class ComponentB extends Component {
     render() {
         return (
             <div>
-                COMPONENTE B
+               <p>Bienvenido, Usuario!</p>
+                <button>Cerrar Sesión</button>
             </div>
         );
     }
@@ -27,11 +57,11 @@ function useConditionalRendering(mostrarA){
 class ConditionalSection extends Component {
     constructor(){
         super();
-        this.state = { mostrarA: true }
+        this.state = { isUserLogged: true }
     }
     render() {
         // const conditionalComponent = useConditionalRendering(this.state.mostrarA)
-        const conditionalComponent = this.state.mostrarA ? <ComponentA/> : <ComponentB/>
+        const conditionalComponent = this.state.isUserLogged ? <ComponentA/> : <ComponentB/>
         return (
             <div>
                 <h4>Conditional Section</h4>
